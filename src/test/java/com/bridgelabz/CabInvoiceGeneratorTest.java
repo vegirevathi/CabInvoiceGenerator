2c4d2d53b2b26e0634c66b330ba951d6a079bef5
vegirevathi <vegirevathi@gmail.com>
2020-07-10 17:37:05 +0530

    ADDED: Test method to calculate mimimum fare

--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 public class CabInvoiceGeneratorTest {
 
     @Test
-    public void givenInvoiceGeneratorShouldReturnTotalFare() {
+    public void givenInvoiceGenerator_ShouldReturnTotalFare() {
         CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double distance = 5.0;
         int time = 5;
         double totalFare = cabInvoiceGenerator.totalFare(distance, time);
         Assert.assertEquals(55,totalFare,0.0);
+
+    }
+
+    @Test
+    public void givenInvoiceGenerator_ShouldReturnMinimumFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 2;
+        double totalFare = cabInvoiceGenerator.totalFare(distance, time);
+        Assert.assertEquals(5,totalFare,0.0);
 
     }
 }
