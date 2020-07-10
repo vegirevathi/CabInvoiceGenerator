af358482303347444f48c5796100111b8a8b664b
vegirevathi <vegirevathi@gmail.com>
2020-07-10 17:38:43 +0530

    ADDED: Condition in main method to calculate minimum fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -4,8 +4,10 @@ public class CabInvoiceGenerator {
 
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final int COST_PER_MINUTE = 1;
+    private static final double MINIMUM_FARE = 5;
 
     public double totalFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
