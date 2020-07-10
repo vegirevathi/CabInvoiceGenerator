c096759208444e5526bcf6d2a7772168415bdc39
vegirevathi <vegirevathi@gmail.com>
2020-07-10 17:16:28 +0530

    ADDED: Main method to calculate total fare

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,7 +1,11 @@
 package com.bridgelabz;
 
 public class CabInvoiceGenerator {
+
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_MINUTE = 1;
+
     public double totalFare(double distance, int time) {
-        return 0;
+        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
     }
 }
